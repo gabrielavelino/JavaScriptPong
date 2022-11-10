7c20c79114f4e6df65469ecba933a20f796b08fe
gabrielavelino <blackzinh8@gmail.com>
2022-11-10 15:15:34 -0300

    Adicionando função para adicionar pacientes

--- DentaIO/js/imc.js
+++ DentaIO/js/imc.js
@@ -14,20 +14,20 @@ function calculoIMC() {
     var pacientes = document.querySelectorAll(".paciente");
 
     for(var i = 0; i < pacientes.length; i++){
-        console.log(pacientes[i].textContent);
+        //console.log(pacientes[i].textContent);
 
         var paciente = pacientes[i];
         var tdPeso = paciente.querySelector(".info-peso");
         var tdAltura = paciente.querySelector(".info-altura");
 
 
-        console.log(paciente.textContent);
-        console.log(tdPeso.textContent);
-        console.log(tdAltura.textContent);
+        // console.log(paciente.textContent);
+        // console.log(tdPeso.textContent);
+        // console.log(tdAltura.textContent);
 
         var imc = IMC();
 
-        console.log(imc);
+        //console.log(imc);
 
         var tdImc = paciente.querySelector(".info-imc");
 
@@ -43,8 +43,50 @@ calculoIMC();
 
 var adicionarPaciente = document.querySelector(".enviar");
 
-adicionarPaciente.addEventListener("click",msn);
+adicionarPaciente.addEventListener("click",adicionaPaciente);
 
-function msn(){
+function adicionaPaciente(event){
     alert("Paciente adicionado");
+    event.preventDefault();
+
+    // Pega os valores do formulario e coloca em uma variável
+    var form = document.querySelector(".formulario");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // console.log(nome);
+    // console.log(peso);
+    // console.log(altura);
+    // console.log(gordura);
+
+    // Cria uma tag tr
+    var pacienteTr = document.createElement("tr");
+
+    // cria uma tag td
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // adiciona o conteuna na tag
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculoIMC();
+
+    // adicionando a tag td para filho de tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    // console.log(pacienteTr);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
 }
