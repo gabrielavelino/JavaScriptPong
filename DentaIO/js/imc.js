df4861b00d624e55aa9d2a1ecb8cbf6220c41bf0
gabrielavelino <blackzinh8@gmail.com>
2022-11-10 14:45:08 -0300

    Adicionando formulario na pagina

--- DentaIO/js/imc.js
+++ DentaIO/js/imc.js
@@ -4,33 +4,47 @@ console.log(titulo.textContent);
 //titulo.textContent = "TESTE";
 
 //Caculo IMC
-function IMC(){
-    var imc = tdPeso.textContent/(tdAltura.textContent*tdAltura.textContent);
-    return imc;
+function calculoIMC() {
+
+    function IMC(){
+        var imc = tdPeso.textContent/(tdAltura.textContent*tdAltura.textContent);
+        return imc;
+    }
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+    for(var i = 0; i < pacientes.length; i++){
+        console.log(pacientes[i].textContent);
+
+        var paciente = pacientes[i];
+        var tdPeso = paciente.querySelector(".info-peso");
+        var tdAltura = paciente.querySelector(".info-altura");
+
+
+        console.log(paciente.textContent);
+        console.log(tdPeso.textContent);
+        console.log(tdAltura.textContent);
+
+        var imc = IMC();
+
+        console.log(imc);
+
+        var tdImc = paciente.querySelector(".info-imc");
+
+        tdImc.textContent = imc.toFixed(2);
+
+        paciente.classList.add("tabelasCertas")
+
+    }
+
 }
-
-var pacientes = document.querySelectorAll(".paciente");
-
-for(var i = 0; i < pacientes.length; i++){
-    console.log(pacientes[i].textContent);
-
-    var paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
-
-
-    console.log(paciente.textContent);
-    console.log(tdPeso.textContent);
-    console.log(tdAltura.textContent);
-
-    var imc = IMC();
-
-    console.log(imc);
-
-    var tdImc = paciente.querySelector(".info-imc");
-
-    tdImc.textContent = parseInt(imc);
-
-}
-
+calculoIMC();
 // Fim
+
+var adicionarPaciente = document.querySelector(".enviar");
+
+adicionarPaciente.addEventListener("click",msn);
+
+function msn(){
+    alert("Paciente adicionado");
+}
