e657a0f34508b40471fbe0a266d5e6e59cab8468
gabrielavelino <blackzinh8@gmail.com>
2022-11-18 11:24:22 -0300

    Iniciando projeto React adicionando header

--- java_react/src/App.js
+++ java_react/src/App.js
@@ -1,24 +1,28 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header className="App-header">
+              <div class="container">
+                <img src={logo} className="App-logo" alt='logo'></img>
+                <div class="caixa">
+                            <nav>
+                    <h1 class="titulo">Denta.io - Sistema para controle de clínicas</h1>
+                                <ul>
+                                    <li><a href="index.html" class="href">Home</a></li>
+                                    <li><a href="produtos.html" class="href">Serviços</a></li>
+                                    <li><a href="contatos.html" class="href">Contatos</a></li>
+                                </ul>
+                            </nav>
+                </div>
+              </div>
+            </header>
+      </div>
   );
 }
 
