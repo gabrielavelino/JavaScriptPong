646e9758e884bd4f6cb5c5fd195fdedda2e078f6
gabrielavelino <blackzinh8@gmail.com>
2022-11-20 16:50:36 -0300

    Estilizando o header

---
+++ java_react/src/componentes/Logo/index.js
@@ -0,0 +1,11 @@
+import logo from '../../logo.svg'
+import React from 'react'
+import './estilo.css'
+
+function Logo(){
+    return (
+        <img src={logo} className="App-logo" alt='logo'></img>
+    )
+}
+
+export default Logo;
