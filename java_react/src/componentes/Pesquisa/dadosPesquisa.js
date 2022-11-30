a441cf14e8a18a5e96c3015e0aa8019b65435b6e
gabrielavelino <blackzinh8@gmail.com>
2022-11-29 23:36:10 -0300

    Adicionando barra de pesquisa

---
+++ java_react/src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,13 @@
+export const Pacientes = [
+    {nome: 'Gabriel Avelino', id: 1},
+    {nome: 'Selena Gomez', id: 2},
+    {nome: 'Joao Paulo', id: 3},
+    {nome: 'Saulo Rafael', id: 4},
+    {nome: 'Ítalo Serra', id: 5},
+]
+
+export const Dentistas = [
+    {nome: 'Jessica Gomes', id: 10},
+    {nome: 'Fausto Emanuel', id: 11},
+    {nome: 'Guilherme Takahashi', id: 12},
+]
--- java_react/src/componentes/Pesquisa/index.js
+++ java_react/src/componentes/Pesquisa/index.js
@@ -1,17 +1,42 @@
 import React from "react"
 import styled from "styled-components";
+import { useState } from "react";
+import { Pacientes } from './dadosPesquisa'
 
 const Input = styled.input`
-    padding = 20px 140px;
+    padding: 15px 50px;
+    border-radius:30px;
+    width: 100px;
+    margin-bottom: 30px;
+    
+`
+
+const PesquisaContainer = styled.section`
+    background-color: white;
+    text-align: center;
+    padding: 20px 0;
+    width: 100%;
 `
 
 function Pesquisa(){
+    const [pessoaPesquisada, setPessoaPesquisada] = useState([])
+    console.log(pessoaPesquisada)
+    function fazPesquisa(evento){
+        const textoDigitado = evento.target.value
+        const resultadoPesquisa = Pacientes.filter(paciente => paciente.nome.includes(textoDigitado))
+        setPessoaPesquisada(resultadoPesquisa)
+    }
+
     return(
-        <section>
+        <PesquisaContainer>
             <h2>Já sabe por onde começar?</h2>
             <h3>Encontre o seu paciente.</h3>
-            <Input placeholder="Digite o paciente"></Input>
-        </section>
+            <Input placeholder="Digite o paciente" onBlur={evento => fazPesquisa(evento)}>
+            </Input>
+            {pessoaPesquisada.map(pessoa => (
+                <p>{pessoa.nome}</p>
+                ))}
+        </PesquisaContainer>
     )
 }
 
